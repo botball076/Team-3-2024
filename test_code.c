b332edd49aa83c73d78fa2a14bbbf45cc5c65800
botball076 <102452299+botball076@users.noreply.github.com>
2024-02-03 09:15:13 -0800

    Changed the printf

--- test_code.c
+++ test_code.c
@@ -3,14 +3,13 @@
 
 int main()
 {
-  printf("Hello, World!\n");
+  printf("Johnny needs to code\n");
   motor(0,100);
   motor(3,100);
   msleep(3000);
   motor(0,100);
   motor(3,0);
   msleep(2000);
-  set_servo_position(0,);
-
+  
   return 0;
 }
