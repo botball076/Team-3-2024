fa7fc45591af703b8508357282a1c25ec27dd0fc
botball076 <102452299+botball076@users.noreply.github.com>
2024-03-27 17:57:49 -0700

    Update orangePomsMission.c

--- orangePomsMission.c
+++ orangePomsMission.c
@@ -2,9 +2,21 @@
 
 void orangePomsMission(){
 
-	motor(0,100);
-	motor(3,100);
-    
+motor(0,50);
+ motor(3,50);
+ msleep(3000);
+ motor(0,60);
+ motor(3,50);
+ msleep(1000);
+ motor(3,50);
+ motor(0,50);
+ msleep(4000);
+ motor(3,50);
+ motor(0,0);
+ msleep(2000);
+ motor(3,50);
+ motor(0,50);
+
 
 
 
